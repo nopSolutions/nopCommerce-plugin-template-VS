da189289c6b1501b7128ef69f063416068cdbbd8
skoshelev <serega.dn@gmail.com>
2019-08-15 12:53:38 +0300

    Added template for nopCommerce 4.10

---
+++ Nop.Plugin Template 4.10/Data/CustomTableMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Nop.Data.Mapping;
+using $safeprojectname$.Domains;
+
+namespace $safeprojectname$.Data
+{
+    public partial class CustomTableMap : NopEntityTypeConfiguration<CustomTable>
+    {
+        public override void Configure(EntityTypeBuilder<CustomTable> builder)
+        {
+            builder.ToTable(nameof(CustomTable));
+
+            builder.HasKey(rcoh => rcoh.Id);
+        }
+    }
+}
