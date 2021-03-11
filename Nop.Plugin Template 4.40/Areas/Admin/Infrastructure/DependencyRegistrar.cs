9a27728840630699259e799bd3579e874f56fc21
skoshelev <sergey.k@nopcommerce.com>
2021-03-11 15:57:50 +0300

    Added template for nopCommerce 4.40

---
+++ Nop.Plugin Template 4.40/Areas/Admin/Infrastructure/DependencyRegistrar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Configuration;
+using Nop.Core.Infrastructure;
+using Nop.Core.Infrastructure.DependencyManagement;
+
+namespace $safeprojectname$.Areas.Admin.Infrastructure
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        /// <summary>
+        /// Register services and interfaces
+        /// </summary>
+        /// <param name="services">Collection of service descriptors</param>
+        /// <param name="typeFinder">Type finder</param>
+        /// <param name="appSettings">App settings</param>
+        public void Register(IServiceCollection services, ITypeFinder typeFinder, AppSettings appSettings)
+        {
+            //services.AddScoped<CustomModelFactory, ICustomerModelFactory>();
+        }
+
+        public int Order => 1;
+    }
+}
---
+++ Nop.Plugin Template 4.40/CustomPlugin.cs
@@ -0,0 +1,14 @@
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+using System.Collections.Generic;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Rename this file and change to the correct type
+    /// </summary>
+    public class CustomPlugin : BasePlugin
+    {
+    }
+}
